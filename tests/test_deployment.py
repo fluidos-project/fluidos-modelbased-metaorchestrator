8227bcbaeece97619f8f1309d333604fa6ac5b1e
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2024-01-24 18:20:45 +0100

    initial
    Signed-off-by: Stefano Braghin <527806+stefano81@users.noreply.github.com>

--- tests/test_deployment.py
+++ tests/test_deployment.py
@@ -1,41 +1,54 @@
 import subprocess
 import time
 from kopf.testing import KopfRunner
+import pytest
+from pytest_kind import KindCluster
+from pathlib import Path
 
 
-def teardown_function(function):
-    subprocess.run("kubectl get mbd | awk '{print $1}' | tail -n+2 | xargs kubectl delete mbd", shell=True, check=True)
-    # subprocess.run("kubectl get pod | awk '{print $1}' | tail -n+2 | xargs kubectl delete pod ", shell=True, check=True)  # not required anymore
-    subprocess.run("kubectl get solvers | awk '{print $1}' | tail -n+2 | xargs kubectl delete solvers", shell=True, check=True)
-    subprocess.run("kubectl get peeringcandidates | awk '{print $1}' | tail -n+2 | xargs kubectl delete peeringcandidates", shell=True, check=True)
+kind_cluster: KindCluster = None
 
 
-def setup_function(function):
-    subprocess.run("kubectl get mbd | awk '{print $1}' | tail -n+2 | xargs kubectl delete mbd", shell=True, check=True)
-    # subprocess.run("kubectl get pod | awk '{print $1}' | tail -n+2 | xargs kubectl delete pod ", shell=True, check=True)  # not required anymore
-    subprocess.run("kubectl get solvers | awk '{print $1}' | tail -n+2 | xargs kubectl delete solvers", shell=True, check=True)
-    subprocess.run("kubectl get peeringcandidates | awk '{print $1}' | tail -n+2 | xargs kubectl delete peeringcandidates", shell=True, check=True)
+def setup_module(module):
+    kind_cluster = KindCluster("foo-bar")
+    kind_cluster.create()
+    time.sleep(3)
+    kind_cluster.kubectl("apply", "-f", Path(
+        Path(__file__).parent.parent,
+        "utils/fluidos-deployment-crd.yaml").absolute().as_posix())
+
+    module.kind_cluster = kind_cluster
+
+
+def teardown_module(module):
+    module.kind_cluster.delete()
 
 
 def test_operator_executes():
     with KopfRunner(["run", "--verbose", "-m", "fluidos_model_orchestrator"]) as runner:
+        print(runner)
         pass
 
     assert runner.exit_code == 0
 
 
-def test_scheduling_successfull_single_pod():
+def test_get_fd_succeds():
     with KopfRunner(["run", "--verbose", "-m", "fluidos_model_orchestrator"]) as runner:
-        subprocess.run("kubectl apply -f tests/examples/test-pod.yaml", shell=True, check=True)
-        time.sleep(5)
+        assert not kind_cluster.kubectl("get", "fd")
 
     assert runner.exit_code == 0
-    print(runner.output)
 
 
-def test_scheduling_successfull_deployment_single_pod():
+def test_scheduling_successfull_single_pod():   #(kind_cluster: KindCluster):
     with KopfRunner(["run", "--verbose", "-m", "fluidos_model_orchestrator"]) as runner:
-        subprocess.run("kubectl apply -f tests/examples/test-deployment.yaml", shell=True, check=True)
-        time.sleep(5)
+        assert not kind_cluster.kubectl("get", "fd")
 
     assert runner.exit_code == 0
+
+
+# def test_scheduling_successfull_deployment_single_pod():
+#     with KopfRunner(["run", "--verbose", "-m", "fluidos_model_orchestrator"]) as runner:
+#         subprocess.run("kubectl apply -f tests/examples/test-deployment.yaml", shell=True, check=True)
+#         time.sleep(5)
+
+#     assert runner.exit_code == 0
