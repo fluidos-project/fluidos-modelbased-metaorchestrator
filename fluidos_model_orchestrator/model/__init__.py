b0fa65b7e86ddb143b7263ecedefc57829bda973
Killian Levacher <killian.levacher@ibm.com>
2024-02-28 15:46:44 +0000

    first version of 2t test working

--- fluidos_model_orchestrator/model/__init__.py
+++ fluidos_model_orchestrator/model/__init__.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 _model_instances: dict[str, ModelInterface] = {
     "dummy": DummyOrchestrator(),
-    "candidate_generation": CGOrchestrator()
+    "candidate_generation": CGOrchestrator(),
 }
 
 
@@ -29,20 +29,45 @@ def convert_to_model_request(spec: Any) -> ModelPredictRequest:
     logger.info("Converting incoming custom resource to model request")
     if spec["kind"] == "Deployment":
         logger.debug("Processing Deployment object")
-        return ModelPredictRequest(id=f"{spec['metadata']['name']}", pod_request=spec["spec"]["template"], intents=_extract_intents(spec["metadata"].get("annotations", {})))
+        return ModelPredictRequest(
+            id=f"{spec['metadata']['name']}",
+            pod_request=spec["spec"]["template"],
+            intents=_extract_intents(spec["metadata"].get("annotations", {})),
+        )
 
     if spec["kind"] == "Pod":
         logger.debug("Processing Pod object")
-        return ModelPredictRequest(id=spec["metadata"]["name"], pod_request=spec, intents=_extract_intents(spec["metadata"].get("annotations", {})))
+
+        # pod_dict["spec"]["containers"][0]["resources"]["requests"]
+        intents = _extract_intents(spec["metadata"].get("annotations", {}))
+        intents.extend(
+            _extract_resource_intents(
+                spec["spec"]["containers"][0]["resources"]["requests"]
+            )
+        )
+
+        return ModelPredictRequest(
+            id=spec["metadata"]["name"],
+            pod_request=spec,
+            intents=intents,
+        )
 
     logger.error(f"Unsupported kind {spec['kind']}")
     raise PermanentError(f"Unsupported kind {spec['kind']}")
 
 
+def _extract_resource_intents(requests: dict[str, str]) -> list[Intent]:
+    logger.debug("Extracting resources intents from request")
+    # pod_dict["spec"]["containers"][0]["resources"]["requests"]
+    return [Intent("cpu", requests["cpu"]), Intent("memory", requests["memory"])]
+
+
 def _extract_intents(annotations: dict[str, str]) -> list[Intent]:
     logger.debug("Extracting intens from annotations")
     intents = [
-        Intent(_extract_intent_name(key).casefold(), str(value).casefold()) for key, value in annotations.items() if key.casefold().startswith("fluidos-intent-")
+        Intent(_extract_intent_name(key).casefold(), str(value).casefold())
+        for key, value in annotations.items()
+        if key.casefold().startswith("fluidos-intent-")
     ]
 
     logger.debug(f"Extracted {len(intents)} intents from the annotations")
