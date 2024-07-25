9200b27359d4b86687d6392e4f60d4b4b929bdeb
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2024-07-25 02:06:24 +0100

    execute daemon only if flavor is locally owned

--- fluidos_model_orchestrator/daemons_and_times/__init__.py
+++ fluidos_model_orchestrator/daemons_and_times/__init__.py
@@ -9,11 +9,24 @@ import kopf  # type: ignore
 from fluidos_model_orchestrator.configuration import CONFIGURATION
 
 
-@kopf.daemon("flavors", cancellation_timeout=1.0)  # type: ignore
-async def daemons_for_flavours_observation(uid: str | None, stopped: bool, logger: Logger, **kwargs: dict[str, Any]) -> None:
+def _check_identity(local_identity: dict[str, str], flavor_owner: dict[str, str]) -> bool:
+    return all(
+        local_identity[key] == flavor_owner.get(key, "")
+        for key in local_identity.keys()
+    )
+
+
+@kopf.daemon("flavours", cancellation_timeout=1.0)  # type: ignore
+async def daemons_for_flavours_observation(uid: str | None, stopped: bool, logger: Logger, spec: dict[str, Any], **kwargs: dict[str, Any]) -> None:
     try:
+        logger.info(f"Running timeseries generation for local flavors only (aka owned by {CONFIGURATION.identity})")
+        if not _check_identity(CONFIGURATION.identity, spec["owner"]):
+            logger.info("Not locally managed flavor, exit")
+            return
+
         while True:
             logger.info(f"Repeating observation for {uid}")
+            logger.info(f"Spec: {spec}")
             if stopped:
                 logger.info("Stopped by external")
                 return
