5cc46a3e45796bfa85b3c196b31396cb493567f3
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2024-08-05 22:25:28 +0100

    cleanup

--- fluidos_model_orchestrator/model/carbon_aware/fakers/weather_forecast_generator.py
+++ fluidos_model_orchestrator/model/carbon_aware/fakers/weather_forecast_generator.py
@@ -1,11 +1,7 @@
 import random
-from typing import List
-
-from fluidos_model_orchestrator.model.carbon_aware.classes.carbon_aware_timeslot import CarbonAwareTimeslot
 
 
-def generate_electricity_maps_forecast(deadline: int):
-    avgCarbonIntensities = []
-    for hour in range(deadline):
-        avgCarbonIntensities.append(random.randint(10, 1000))
-    return avgCarbonIntensities
+def generate_electricity_maps_forecast(deadline: int) -> list[int]:
+    return [
+        random.randint(10, 1_000) for _ in range(deadline)
+    ]
