f3896b7611a751c299ffcd05cd87bf59b060b282
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2024-09-30 05:19:01 +0100

    fix creation of config map information

--- fluidos_model_orchestrator/configuration.py
+++ fluidos_model_orchestrator/configuration.py
@@ -6,6 +6,7 @@ from typing import Any
 import kopf  # type: ignore
 from kubernetes.client import CoreV1Api  # type: ignore
 from kubernetes.client import V1ConfigMapList
+from kubernetes.client import V1SecretList
 from kubernetes.client.api_client import ApiClient  # type: ignore
 from kubernetes.client.configuration import Configuration as K8SConfig  # type: ignore
 from kubernetes.client.exceptions import ApiException  # type: ignore
@@ -41,10 +42,70 @@ def enrich_configuration(config: Configuration,
     config.k8s_client = _build_k8s_client(my_config)
     config.identity = _retrieve_node_identity(config, logger)
     config.api_keys = _retrieve_api_key(config, logger)
+    config.architecture = _retrieve_architecture(config, logger)
+
+
+def _retrieve_api_key_from_secret(config: Configuration, logger: logging.Logger) -> dict[str, str]:
+    logger.info("Retrieving API KEYS from secret")
+    api_endpoint = CoreV1Api(config.k8s_client)
+
+    try:
+        secrets: V1SecretList = api_endpoint.list_secret_for_all_namespaces()
+        if len(secrets.items):
+            for secret in secrets.items:
+                if secret.metadata is None:
+                    continue
+
+                if secret.metadata.name == "electricity-map":
+                    logger.info("Secret identified")
+                    if secret.data is None:
+                        raise ValueError("Secret data missing.")
+
+                    data: dict[str, str] = secret.data
+                    return {
+                        "ELECTRICITY_MAP_API_KEY": data.get("KEY", "PLACEHOLDER_NOT_VALID")
+                    }
+    except ApiException as e:
+        logger.error(f"Unable to retrieve secrets {e=}")
+
+    logger.error("Something went wrong while retrieving secrets")
+    raise ValueError("Unable to retrieve secrets")
 
 
 def _retrieve_api_key(config: Configuration, logger: logging.Logger) -> dict[str, str]:
     logger.info("Retrieving API KEYS from config map")
+    api_endpoint = CoreV1Api(config.k8s_client)
+
+    try:
+        config_maps: V1ConfigMapList = api_endpoint.list_config_map_for_all_namespaces()
+        if len(config_maps.items):
+            for config_map in config_maps.items:
+                if config_map.metadata is None:
+                    continue
+
+                if config_map.metadata.name == "fluidos-mbmo-configmap":
+                    logger.info("ConfigMap identified")
+                    if config_map.data is None:
+                        raise ValueError("ConfigMap data missing.")
+
+                    data: dict[str, str] = config_map.data
+                    return {
+                        key: value
+                        for key, value in data.items() if key.endswith("_API_KEY")
+                    }
+    except ApiException as e:
+        logger.error(f"Unable to retrieve config map {e=}")
+
+    logger.error("Something went wrong while retrieving config map")
+    raise ValueError("Unable to retrieve config map")
+
+
+def _build_k8s_client(config: K8SConfig) -> ApiClient:
+    return ApiClient(config)
+
+
+def _retrieve_architecture(config: Configuration, logger: logging.Logger) -> str:
+    logger.info("Retrieving archicture from config map")
     api_endpoint = CoreV1Api(config.k8s_client)
 
     try:
@@ -60,19 +121,13 @@ def _retrieve_api_key(config: Configuration, logger: logging.Logger) -> dict[str
                         raise ValueError("ConfigMap data missing.")
 
                     data: dict[str, str] = item.data
-                    return {
-                        key: value
-                        for key, value in data.items() if key.endswith("_API_KEY")
-                    }
+
+                    return data.get("architecture", "amd64")
     except ApiException as e:
         logger.error(f"Unable to retrieve config map {e=}")
 
     logger.error("Something went wrong while retrieving config map")
     raise ValueError("Unable to retrieve config map")
-
-
-def _build_k8s_client(config: K8SConfig) -> ApiClient:
-    return ApiClient(config)
 
 
 def _retrieve_node_identity(config: Configuration, logger: logging.Logger) -> dict[str, str]:
