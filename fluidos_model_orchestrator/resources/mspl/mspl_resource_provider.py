9e5198efff83928089295319a6e0023da52a23ee
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2024-10-24 01:00:27 +0100

    add missing file

---
+++ fluidos_model_orchestrator/resources/mspl/mspl_resource_provider.py
@@ -0,0 +1,25 @@
+from typing import Any
+
+from fluidos_model_orchestrator.common import ExternalResourceProvider
+from fluidos_model_orchestrator.common import Intent
+from fluidos_model_orchestrator.common import KnownIntent
+from fluidos_model_orchestrator.configuration import CONFIGURATION
+from fluidos_model_orchestrator.resources.mspl import request_application
+
+
+class MSPLIntentWrapper(ExternalResourceProvider):
+    def __init__(self, intent: Intent):
+        if intent.name is not KnownIntent.mspl:
+            raise ValueError("Expected MSPL intent")
+        self.intent = intent
+
+    def enrich(self, container: dict[str, Any]) -> None:
+        response = request_application(
+            policy=self.intent.value,
+            endpoint=CONFIGURATION.MSPL_ENDPOINT
+        )
+
+        if response == "ALL GOOD":
+            return None
+        else:
+            raise ValueError("Something went wrong")
