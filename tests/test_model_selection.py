2fba8a1b231e3b7cdbdd885e910f680bb97081a6
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2025-03-30 00:20:06 +0000

    fix key issue

--- tests/test_model_selection.py
+++ tests/test_model_selection.py
@@ -5,6 +5,7 @@ from fluidos_model_orchestrator.model import convert_to_model_request
 from fluidos_model_orchestrator.model import get_model_object
 from fluidos_model_orchestrator.model.candidate_generation.model import Orchestrator as CandidateGeneration
 from fluidos_model_orchestrator.model.carbon_aware.orchestrator import CarbonAwareOrchestrator
+from fluidos_model_orchestrator.model.ensemble import FluidosModelEnsemble
 
 
 def test_something_is_returned_even_with_no_intents():
@@ -21,3 +22,16 @@ def test_returns_most_matching():
     assert spec is not None
 
     assert type(get_model_object(spec)) is CandidateGeneration
+
+
+def test_returns_ensamble():
+    with pkg_resources.resource_stream(__name__, "k8s/rse-example.yaml") as stream:
+        spec = convert_to_model_request(
+            yaml.safe_load(stream)["spec"],
+            "fluidos"
+        )
+    assert spec is not None
+
+    m = get_model_object(spec)
+
+    assert type(m) is FluidosModelEnsemble
