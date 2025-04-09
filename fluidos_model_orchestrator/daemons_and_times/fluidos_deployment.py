e710cdebae199e5bf1302241ffdccd774695af9d
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2025-04-09 14:00:06 +0100

    add check for intents needing monitoring

--- fluidos_model_orchestrator/daemons_and_times/fluidos_deployment.py
+++ fluidos_model_orchestrator/daemons_and_times/fluidos_deployment.py
@@ -6,11 +6,28 @@ import kopf  # type: ignore
 from kopf._cogs.structs import bodies  # type: ignore
 from kopf._cogs.structs import patches  # type: ignore
 
+from fluidos_model_orchestrator.common import Intent
+from fluidos_model_orchestrator.common import ModelPredictRequest
 from fluidos_model_orchestrator.configuration import CONFIGURATION
+from fluidos_model_orchestrator.model import convert_to_model_request
 
 
-def requires_monitoring(spec: dict[str, Any]) -> bool:
-    return True
+def requires_validation(intent: Intent) -> bool:
+    return intent.needs_monitoring()
+
+
+def requires_monitoring(spec: dict[str, Any], namespace: str | None) -> list[Intent]:
+    if namespace is None:
+        namespace = "default"
+    request: ModelPredictRequest | None = convert_to_model_request(spec, namespace)
+
+    if request is None:
+        return []
+
+    return [
+        intent for intent in request.intents
+        if requires_validation(intent)
+    ]
 
 
 @kopf.daemon("fluidosdeployments")  # type: ignore
@@ -34,7 +51,7 @@ async def daemons_for_fluidos_deployment(
         param: Any,
         **kwargs: dict[str, Any]) -> None:
     # check if the spec require monitoring (based on the intents)
-    if not requires_monitoring(spec):
+    if not requires_monitoring(spec, namespace):
         return
 
     while not stopped:
