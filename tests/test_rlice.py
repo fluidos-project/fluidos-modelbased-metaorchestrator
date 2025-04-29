7760563c77d61628def214e9b97dd18f685870d4
doriana <doriana2405@gmail.com>
2025-04-29 11:01:15 +0000

    load model from huggingface

--- tests/test_rlice.py
+++ tests/test_rlice.py
@@ -1,3 +1,8 @@
+import pkg_resources
+import yaml
+
+from fluidos_model_orchestrator.model import convert_to_model_request
+from fluidos_model_orchestrator.model import get_model_object
 from fluidos_model_orchestrator.common import Flavor
 from fluidos_model_orchestrator.common import FlavorCharacteristics
 from fluidos_model_orchestrator.common import FlavorK8SliceData
@@ -7,7 +12,12 @@ from fluidos_model_orchestrator.common import FlavorType
 from fluidos_model_orchestrator.common import FlavorTypeData
 from fluidos_model_orchestrator.common import GPUData
 from fluidos_model_orchestrator.common import Intent
+from fluidos_model_orchestrator.common import KnownIntent
+from fluidos_model_orchestrator.common import ModelPredictRequest
+from fluidos_model_orchestrator.common import ModelPredictResponse
+from fluidos_model_orchestrator.common import Resource
 from fluidos_model_orchestrator.resources.rear.local_resource_provider import LocalResourceProvider
+from fluidos_model_orchestrator.model.rlice.model import RliceOrchestrator
 
 def test_validate_provider_characteristics() -> None:
 
@@ -67,3 +77,25 @@ def test_validate_provider_characteristics() -> None:
             }
         )
     ))
+
+    providers: list[ResourceProvider] = [bad_no_price,good]
+
+    with pkg_resources.resource_stream(__name__, "examples/test-pod-w-intent.yaml") as stream:
+        request = convert_to_model_request(
+            yaml.safe_load(stream)["spec"],
+            "fluidos"
+        )
+        print(request)
+    prediction = ModelPredictResponse(
+        "resp-123",
+        Resource(id="res-123", architecture="amd64", cpu="250m", memory="64Mi"),
+        delay=0
+    )
+    orchestrator = RliceOrchestrator()
+
+    ranked = orchestrator.rank_resources(providers, prediction, request)
+
+    #assert ranked is not None
+    #assert len(ranked) == 1
+
+test_validate_provider_characteristics()
