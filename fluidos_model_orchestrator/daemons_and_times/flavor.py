307ccdfedeed493ca176b54241df39161dc26f4c
Stefano Braghin <527806+stefano81@users.noreply.github.com>
2025-06-18 02:53:10 +0100

    fix flavor update

--- fluidos_model_orchestrator/daemons_and_times/flavor.py
+++ fluidos_model_orchestrator/daemons_and_times/flavor.py
@@ -1,17 +1,18 @@
 import datetime
 from logging import Logger
 from typing import Any
+from typing import cast
 
 import kopf  # type: ignore
 from kopf._cogs.structs import bodies  # type: ignore
 from kopf._cogs.structs import patches  # type: ignore
 
 from fluidos_model_orchestrator.common import build_flavor
+from fluidos_model_orchestrator.common import ResourceFinder
 from fluidos_model_orchestrator.configuration import CONFIGURATION
+from fluidos_model_orchestrator.flavor import FlavorK8SliceData
 from fluidos_model_orchestrator.model.carbon_aware.forecast_updater import update_local_flavor_forecasted_data
-# from typing import cast
-# from fluidos_model_orchestrator.flavor import FlavorK8SliceData
-# from fluidos_model_orchestrator.resources import get_resource_finder
+from fluidos_model_orchestrator.resources import get_resource_finder
 
 
 @kopf.daemon("flavors", cancellation_timeout=1.0)  # type: ignore
@@ -34,12 +35,13 @@ async def daemons_for_flavours_observation(
         memo: Any,
         param: Any,
         **kwargs: dict[str, Any]) -> None:
-
     logger.info(f"Running timeseries generation for local flavors only (aka owned by {CONFIGURATION.identity})")
 
     if not CONFIGURATION.check_identity(spec["owner"]):
         logger.info("Flavor {}{} is not managed locally. Exit", namespace, name)
         return
+
+    finder: ResourceFinder | None = None
 
     while not stopped:
         stopped.wait(CONFIGURATION.FLAVOR_UPDATE_SLEEP_TIME)
@@ -61,7 +63,7 @@ async def daemons_for_flavours_observation(
             logger.info("Flavor not updated")
             continue
 
-        logger.info(f"Flavor updated: {update_flavor}")
-        get_resource_finder().update_local_flavor(flavor=flavor,
-                                                  data=cast(FlavorK8SliceData, update_flavor.spec.flavor_type.type_data).properties,
-                                                  namespace=namespace)
+        if finder is None:
+            finder = get_resource_finder()
+
+        finder.update_local_flavor(flavor, cast(FlavorK8SliceData, update_flavor.spec.flavor_type.type_data).properties, namespace)
